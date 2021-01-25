bccdc2da9cdffb513719282bf532dbe772569949
Tanngrisnr <fredrik.wintzell@gmail.com>
2021-01-25 16:54:24 +0100

    working mvp weather widget

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-
-import CurrentStockholm from "./CurrentStockholm";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import CurrentStockholm from "./CurrentStockholm";
 import Contact from './Contact';
 import About from './About';
 import Projects from './Projects';
 import Home from './Home';
 import Navigation from './Navigation';
 
+const Normalize = createGlobalStyle`
+  *{
+    font-family:'Roboto Slab', serif;
+  }
+`;
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q=Stockholm&units=metric&appid=f2eb762353dd6b9f926b406db34c6ae1"
+
 function App() {
+
+  const [data, setData] = useState({ data: null });
+  console.log(data);
+
+  useEffect(() => {
+      console.log("Hej från useEffect");
+      fetch(url)
+          .then(response => response.json())
+          .then(json => setData({ data: json }));
+  }, []);
+
+
   return (
   <Router>
+    <Normalize/>
     <Navigation/>
+    {data.data ? <CurrentStockholm
+    description={data.data.weather[0].description}
+    temp={data.data.main.temp}
+    location={data.data.name}
+    weather_id={data.data.weather[0].id}
+    /> : null}
     <Switch>
       <Route path="/contact"><Contact/></Route>
       <Route path="/about"><About/></Route>
--- src/CurrentStockholm.js
+++ src/CurrentStockholm.js
@@ -1,14 +1,81 @@
 import Clock from "react-live-clock";
-import CurrentWeather from './CurrentWeather'
+import WEATHER_DATA from './data.js'
+import Emoji from 'a11y-react-emoji'
 
-const CurrentStockholm = () => {
+
+
+const CurrentStockholm = ({description, temp, location, weather_id}) => {
+
+    
+/*     const weather = {
+        weather: WEATHER_DATA.weather[0].main,
+        description: WEATHER_DATA.weather[0].description,
+        temp: WEATHER_DATA.main.temp,
+        name: WEATHER_DATA.name,
+        weather_id: WEATHER_DATA.weather[0].id,
+    } */
+    const icon = {
+        emoji: null,
+        label: null
+    };
+    const filterIcon = wData => {
+    
+        if (wData >= 600 && wData <= 622) {
+            icon.emoji = "🌨️"
+            icon.label = "cloud with snow"
+            console.log(icon);
+        }
+        else if (wData >= 500 && wData <= 531){
+            icon.emoji = "🌧️"
+            icon.label = "clould with rain"
+            console.log(icon);
+
+        }
+        else if (wData >= 300 && wData <= 321){
+            icon.emoji = "🌦️"
+            icon.label = "sun behind rain cloud"
+            console.log(icon);
+
+        }
+        else if (wData >= 200 && wData <= 232){
+            icon.emoji = "⛈️"
+            icon.label = "cloud with rain and lightning"
+            console.log(icon);
+
+        }
+        else if (wData >= 701 && wData <= 771){
+            icon.emoji = "🌫️"
+            icon.label = "fog"
+            console.log(icon);
+
+        }
+        else if (wData >= 801 && wData <= 804){
+            icon.emoji = "☁️"
+            icon.label = "cloud"
+            console.log(icon);
+
+        }
+        else if (wData === 800){
+            icon.emoji = "🌤️"
+            icon.label = "sun behind small cloud"
+            console.log(icon);
+
+        }
+    
+    }
+    filterIcon(weather_id);
     return ( 
         <div>
-        <CurrentWeather/>
-        <Clock
+        <ul>
+           <li><Emoji symbol={icon.emoji} label={icon.label}/></li>
+           <li>{description}</li>
+           <li>{temp} C</li>
+           <li>{location}</li>
+           <li><Clock
             format={'HH:mm:ss'}
             ticking={true}
-            timezone={'Europe/Stockholm'} />
+            timezone={'Europe/Stockholm'} /></li>
+       </ul>
       </div>
      );
   }
