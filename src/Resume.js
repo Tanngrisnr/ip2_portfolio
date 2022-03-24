614bb39fddc54daef09121f063f5a0ae55455f91
Tanngrisnr <fredrik.wintzell@gmail.com>
2022-03-24 16:11:04 +0100

    fixed resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,19 +1,21 @@
-import {StyledSection} from './styles/global'
-import List from './list/List'
+import { StyledSection } from "./styles/global";
+import List from "./list/List";
 
-
-
-const Resume = ({data}) => {
-    return ( 
-
-        <StyledSection>
-        <h2>Résumé</h2>
-        <List key="education" list={data[0]}>Education</List>
-        <List key="experience" list={data[1]}>Experience</List>
-        <List key="skills" list={data[2]}>Skills</List>
-        </StyledSection>
-
-     );
-}
+const Resume = ({ data }) => {
+  return (
+    <StyledSection>
+      <h2>Résumé</h2>
+      <List key="experience" list={data[0]}>
+        Experience
+      </List>
+      <List key="education" list={data[1]}>
+        Education
+      </List>
+      <List key="skills" list={data[2]}>
+        Skills
+      </List>
+    </StyledSection>
+  );
+};
 
 export default Resume;
